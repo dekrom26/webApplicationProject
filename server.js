b7305dae8c32ee8ccbe2007e53b4f8ad8b6e8a1c
HadarBasson1 <hadarbsn@gmail.com>
2022-06-21 12:52:32 +0300

    commit 21/06 12:55

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ mongoose.connect('mongodb://localhost:27017/Flights', { useNewUrlParser: true, u
 
     app.get("/flights",async(req,res)=>{
         const flights = await Flights.find({})
-        console.log(flights);
+        // console.log(flights);
+        res.send(flights);
     })
 
     app.get("/",(req,res)=>
@@ -31,6 +32,62 @@ mongoose.connect('mongodb://localhost:27017/Flights', { useNewUrlParser: true, u
     app.get('/cart', function (req, res) {
         res.sendfile(public + '/shoppingCart.html')
       });
+
+    
+
+
+    ///////////////
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(methodOverride('_method'))
+
+
+// app.get('/products', async (req, res) => {
+//     const { category } = req.query;
+//     if (category) {
+//         const products = await Product.find({ category })
+//         res.render('products/index', { products, category })
+//     } else {
+//         const products = await Product.find({})
+//         res.render('products/index', { products, category: 'All' })
+//     }
+// })
+
+// app.get('/products/new', (req, res) => {
+//     res.render('products/new', { categories })
+// })
+
+// app.post('/products', async (req, res) => {
+//     const newProduct = new Product(req.body);
+//     await newProduct.save();
+//     res.redirect(`/products/${newProduct._id}`)
+// })
+
+// app.get('/products/:id', async (req, res) => {
+//     const { id } = req.params;
+//     const product = await Product.findById(id)
+//     res.render('products/show', { product })
+// })
+
+// app.get('/products/:id/edit', async (req, res) => {
+//     const { id } = req.params;
+//     const product = await Product.findById(id);
+//     res.render('products/edit', { product, categories })
+// })
+
+// app.put('/products/:id', async (req, res) => {
+//     const { id } = req.params;
+//     const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+//     res.redirect(`/products/${product._id}`);
+// })
+
+// app.delete('/products/:id', async (req, res) => {
+//     const { id } = req.params;
+//     const deletedProduct = await Product.findByIdAndDelete(id);
+//     res.redirect('/products');
+// })
+
+
 
 app.listen(8080, () => {
     console.log("APP IS LISTENING ON PORT 8080!")
