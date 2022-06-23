81465e34fb82440c42e70edf5905bed9669f91e2
dekel <dekelr26@cs.colman.ac.il>
2022-06-23 10:50:39 +0300

    facebook log in - not working yet

--- public/index.js
+++ public/index.js
@@ -5,6 +5,46 @@ window.onload = function() {
     var img = document.getElementById("imgCanvas");
    ctx.drawImage(img, 0, 0,52,52);
 };
+
+    window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '707420810543094',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v14.0'
+    });
+
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+        });
+
+};
+
+
+    (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+    function statusChangeCallback(response){
+        if(response.status === 'connected'){
+            console.log('Logged in and authenticated');
+        }else{
+            console.log('Not authenticated');
+        }
+    }
+
+
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+}
+
+
 function initMap() {
     // map options.
     let options = {
