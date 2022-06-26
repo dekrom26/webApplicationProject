4a4bc67d174d550d846179b9d199632a71a95c1a
dekel <dekelr26@cs.colman.ac.il>
2022-06-26 10:17:22 +0300

    new token to FB

--- public/index.js
+++ public/index.js
@@ -10,19 +10,20 @@ window.onload = function() {
 document.getElementById("fb-share-button").addEventListener('click', function() {
     let xhr = new XMLHttpRequest();
 // <<<<<<< HEAD
-    var message = "hello1";
+    //var message = "hello1";
 // =======
     var message = window.prompt("תנו לנו ביקורת:");
     // console.log(message);
 // >>>>>>> 81f850a04fe8fb875112bbb792c81eda6f9183d6
-    var accessToken = "EAAPjYG6LzYMBAG00lwAzFMMp8li26nphSjHALtBNAmW8eZALfj2CZC2PQxy79tZCOVHH276w3M5fjBVM5qDULChLCGjUeEak0ELpJXZCCSH367aveQKhQxvlYxYauOt5ZC3OiDRZBo3JJkwybyAQejxWyylifS6cJ4kgZC8ZCbBEF6EwzVKvMXdtncedBGH1uEnkz7jTaSjtkAZDZD"
+    var accessToken = "EAAPjYG6LzYMBAFxo6fbUCHUumyt6ZAceU5YBJPAnvtrZCWQZCOBAoRksMjWESeT7OeJTZBYh5VcLx4VMJO635iQDdbvsdZBBTbnPDwGY6Tgv890pCMw2k9vXg6IpK38k2HLnWNZABiMYOocLkfGsT9e7heduHsZBRADmBPNGPwtjgZDZD\n"
     var createPostRequest = "https://graph.facebook.com/v14.0/104839158945939/feed?message=" + encodeURIComponent(message) + "&access_token=" + accessToken
     xhr.open("POST", createPostRequest);
     xhr.onload = () => console.log(xhr.responseText);
     xhr.send()
 });
 
-
+//curl -i -X POST \
+//  "https://graph.facebook.com/v14.0/104839158945939/feed?message=test1&access_token=EAAPjYG6LzYMBAAtA55ZAXCbjMZCPGq1FXDZBtZB5aCaUKK2ZA8pCQnkdiyBVQgwphGe0ZA7qUSYWjlB13UhnltocSFUHmzdG28HAnZAEEEWKXSoRJl6tnwmuqJCpBH6byuZCRjNpJPC98CnRnsR16bR16ZB3R1mBGueOvHFfuJXLAsCwIpUwyPq4T"
 
 function initMap() {
     // map options.
